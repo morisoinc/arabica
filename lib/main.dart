321a4768a1a3b5d73872d51e2086971eb72e2fba
morisoinc <64343733+morisoinc@users.noreply.github.com>
2024-05-30 20:30:14 -0300

    setting up hydrated blocs in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:arabica/services/router.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getApplicationDocumentsDirectory(),
+  );
   runApp(const ArabicaApp());
 }
 
