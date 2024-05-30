3d768f9f70a4285aeb322ef59d936c7135aacf8a
morisoinc <64343733+morisoinc@users.noreply.github.com>
2024-05-30 20:30:49 -0300

    added favorites screen with remove option

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,4 +1,6 @@
+import 'package:arabica/controller/favorites_bloc/favorites_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({super.key});
@@ -7,8 +9,26 @@ class FavoritesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("faves"),
-    );
+    return BlocBuilder<FavoritesBloc, FavoritesState>(
+        builder: (context, state) {
+      return ListView.builder(
+        itemCount: state.favorites.length,
+        itemBuilder: (context, index) {
+          final favorite = state.favorites[index];
+          return ListTile(
+            title: Text(favorite.url),
+            leading: Image.memory(favorite.image),
+            trailing: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                context
+                    .read<FavoritesBloc>()
+                    .add(FavoritesEvent.removeFavorite(favorite));
+              },
+            ),
+          );
+        },
+      );
+    });
   }
 }
